859bb45152b54684648a9c8ccd621e10957e37aa
yarangodev <yeisonarango03@gmail.com>
2023-09-17 12:15:21 -0500

    fix: modify model of AffiliatesSuscription

--- src/models/affiliatesSuscription.model.js
+++ src/models/affiliatesSuscription.model.js
@@ -6,7 +6,6 @@ const AffiliatesSuscriptionSchema = new Schema({
     _id: Schema.Types.ObjectId,
     idAfiliado: { type: Schema.Types.ObjectId, ref: "affiliates", required: true },
     fechaDePago: { type: Date, required: true },
-    valorDePago: { type: Number, required: true },
     medioDePago: { type: String, required: true },
     activo: { type: Boolean, required: true },
 });
