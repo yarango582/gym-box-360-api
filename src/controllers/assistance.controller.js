c1e03da68b8c2e2f98d3001dc44eaba74dc7b228
yarangodev <yeisonarango03@gmail.com>
2023-09-19 00:12:36 -0500

    fix: module of assistance and decrement diasDeCortesia

--- src/controllers/assistance.controller.js
+++ src/controllers/assistance.controller.js
@@ -3,61 +3,78 @@ const Affiliate = require('../models/affiliates.model');
 const AffiliateSuscription = require('../models/affiliatesSuscription.model');
 
 const createAssistance = async (req, res) => {
-    const reqBody = req.body;
-
-    if (!reqBody) {
-        return res.status(400).json({
-            success: false,
-            error: 'You must provide an assistance',
-        });
-    }
-
-    const affiliate = await Affiliate.findOne({
-        numeroDocumento: reqBody.numeroDocumento,
-    });
-
-    if (!affiliate) {
-        return res.status(400).json({
-            success: false,
-            message: 'El usuario no es un afiliado',
-        });
-    }
-
-    const isAssistanceCreatedSameDay = await Assistance.find({
-        numeroDocumento: reqBody.numeroDocumento,
-        fechaDeAsistencia: {
-            $gte: new Date(reqBody.fechaDeAsistencia).setHours(0, 0, 0),
-            $lt: new Date(reqBody.fechaDeAsistencia).setHours(23, 59, 59),
-        },
-    });
-
-    const isActiveSuscription = await AffiliateSuscription.findOne({
-        idAfiliado: affiliate._id,
-    });
-
-    if (affiliate.diasDeCortesia === 0 && !isActiveSuscription || isActiveSuscription.activo === false) {
-        return res.status(400).json({
-            success: false,
-            message: 'El afiliado no tiene una suscripcion activa',
-        });
-    }
-
-    if (isAssistanceCreatedSameDay.length) {
-        return res.status(400).json({
-            success: false,
-            message: 'No es necesario registrar la asistencia, ya se encuentra registrada',
-        });
-    }
-
-    const assistance = new Assistance(reqBody);
-
-    if (!assistance) {
-        return res.status(400).json({ success: false, error: err });
-    }
-
     try {
+        const reqBody = req.body;
+
+        if (!reqBody) {
+            return res.status(400).json({
+                success: false,
+                error: 'You must provide an assistance',
+            });
+        }
+
+        // valida si el usuario existe
+        const affiliate = await Affiliate.findOne({
+            numeroDocumento: reqBody.numeroDocumento,
+        });
+
+        // valida si el usuario es un afiliado
+        if (!affiliate) {
+            return res.status(400).json({
+                success: false,
+                message: 'El usuario no es un afiliado',
+            });
+        }
+
+        // valida si ya existe una asistencia registrada el mismo dia
+        const isAssistanceCreatedSameDay = await Assistance.find({
+            numeroDocumento: reqBody.numeroDocumento,
+            fechaDeAsistencia: {
+                $gte: new Date(reqBody.fechaDeAsistencia).setHours(0, 0, 0),
+                $lt: new Date(reqBody.fechaDeAsistencia).setHours(23, 59, 59),
+            },
+        });
+
+        // valida si existe una suscripcion
+        const isActiveSuscription = await AffiliateSuscription.findOne({
+            idAfiliado: affiliate._id,
+        });
+
+        // valida si tiene dias de cortesia y si la suscripcion esta activa
+        if (affiliate.diasDeCortesia === 0 && !isActiveSuscription || isActiveSuscription?.activo === false) {
+            return res.status(400).json({
+                success: false,
+                message: 'El afiliado no tiene una suscripcion activa',
+            });
+        }
+
+        // actualiza los dias de cortesia
+        await Affiliate.updateOne({
+            numeroDocumento: reqBody.numeroDocumento,
+        }, {
+            diasDeCortesia: affiliate.diasDeCortesia - 1,
+        });
+
+        // valida si ya existe una asistencia registrada el mismo dia
+        if (isAssistanceCreatedSameDay.length) {
+            return res.status(400).json({
+                success: false,
+                message: 'No es necesario registrar la asistencia, ya se encuentra registrada',
+            });
+        }
+
+        // crea la asistencia
+        const assistance = new Assistance(reqBody);
+
+        // valida si la asistencia se creo correctamente
+        if (!assistance) {
+            return res.status(400).json({ success: false, error: err });
+        }
+
+        // guarda la asistencia
         await assistance.save();
 
+        // retorna la asistencia creada
         return res.status(201).json({
             success: true,
             id: assistance._id,
