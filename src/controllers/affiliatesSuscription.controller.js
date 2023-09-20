5e181199c0dae813d672abc6f9ea7638c7bd4562
yarangodev <yeisonarango03@gmail.com>
2023-09-20 14:22:10 -0500

    feat: add payment history when is created the affiliateSuscription

--- src/controllers/affiliatesSuscription.controller.js
+++ src/controllers/affiliatesSuscription.controller.js
@@ -1,4 +1,5 @@
 const affiliatesSuscriptionModel = require('../models/affiliatesSuscription.model');
+const paymentsModel = require('../models/payments.model');
 
 const createAffiliateSuscriptionOrUpdate = async (req, res) => {
     try {
@@ -16,20 +17,37 @@ const createAffiliateSuscriptionOrUpdate = async (req, res) => {
         // update suscription
         if (isAffiliateSuscriptionCreated) {
             await affiliatesSuscriptionModel.updateOne({ idAfiliado: reqBody.idAfiliado }, reqBody);
+            const payment = new paymentsModel({
+                idAfiliado: reqBody.idAfiliado,
+                fechaDePago: reqBody.fechaDePago,
+                medioDePago: reqBody.medioDePago,
+                valorDePago: reqBody.valorDePago,
+            });
+            await payment.save();
             return res.status(201).json({
                 success: true,
-                message: "Suscripcion actualizada correctamente!",
+                message: "Suscripcion actualizada correctamente y pago registrado!",
             });
         }
 
         // create suscription
         const affiliateSuscription = new affiliatesSuscriptionModel(reqBody);
 
-        if (!affiliateSuscription) {
+        // create payment
+        const payment = new paymentsModel({
+            idAfiliado: reqBody.idAfiliado,
+            fechaDePago: reqBody.fechaDePago,
+            medioDePago: reqBody.medioDePago,
+            valorDePago: reqBody.valorDePago,
+        });
+
+        if (!affiliateSuscription || !payment) {
             return res.status(400).json({ success: false, message: err });
         }
 
         await affiliateSuscription.save();
+        await payment.save();
+
         return res.status(201).json({
             success: true,
             id: affiliateSuscription._id,
